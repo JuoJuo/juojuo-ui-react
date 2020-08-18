5b73528a49914c261759a4d90dc6ff03920d8e08
a996683013@Gmail.com <a996683013@Gmail.com>
2020-08-18 18:39:28 +0800

    day2 record

--- src/stories/components/button/index.tsx
+++ src/stories/components/button/index.tsx
@@ -1,20 +1,88 @@
-import React, { ButtonHTMLAttributes, PropsWithChildren } from "react";
-import styled from "styled-components";
+import React, {AnchorHTMLAttributes, ButtonHTMLAttributes, PropsWithChildren, ReactNode, useMemo} from 'react';
+import { Text, Loading, StyledButton } from './css';
 
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    primary?: boolean;
-    backgroundColor?: string;
-    size?: 'small' | 'medium' | 'large';
-    label?: string;
-    onClick?: () => void;
+type btnType = 'primary' | 'primaryOutline' | 'secondary' | 'secondaryOutline' | 'tertiary' | 'outline' |
+  'inversePrimary' | 'inverseSecondary' | 'inverseOutline';
+
+type AppearancesObj = {
+    [key in btnType]: key;
+};
+
+type sizeType = 'small' | 'medium';
+
+type sizeObj = {
+    [key in sizeType]: key;
+};
+
+export const APPEARANCES: AppearancesObj = {
+    primary: 'primary',
+    primaryOutline: 'primaryOutline',
+    secondary: 'secondary',
+    secondaryOutline: 'secondaryOutline',
+    tertiary: 'tertiary',
+    outline: 'outline',
+    inversePrimary: 'inversePrimary',
+    inverseSecondary: 'inverseSecondary',
+    inverseOutline: 'inverseOutline',
+};
+
+export const SIZES: sizeObj = {
+    small: 'small',
+    medium: 'medium',
+};
+
+export type AppearancesTypes = keyof typeof APPEARANCES;
+export type SizesTypes = keyof typeof SIZES;
+
+export interface CustormButtonProps {
+    /** 是否禁用 */
+    disabled?: boolean;
+    /** 是否加载中 */
+    isLoading?: boolean;
+    /** 是否是a标签 */
+    isLink?: boolean;
+    /** 是否替换加载中文本 */
+    loadingText?: ReactNode;
+    /** 按钮大小 */
+    size?: SizesTypes;
+    /** 按钮类型 */
+    appearance?: AppearancesTypes;
+    /** 无效点击 */
+    isUnclickable?: boolean;
 }
 
-const Mybutton = styled.button({
-    color: "red",
-});
+export type ButtonProps = CustormButtonProps & AnchorHTMLAttributes<HTMLAnchorElement> & ButtonHTMLAttributes<HTMLButtonElement>;
 
 function Button(props: PropsWithChildren<ButtonProps>) {
-    const { children, ...rest } = props;
-    return <Mybutton {...rest}>{children}</Mybutton>;
+    const { isLoading, loadingText, isLink, children } = props;
+    const buttonInner = (
+      <>
+          <Text>{children}</Text>
+          {isLoading && <Loading>{loadingText || 'Loading...'}</Loading>}
+      </>
+    );
+    const btnType = useMemo(() => {
+        if (isLink) {
+            return 'a';
+        }
+    }, [isLink]);
+
+    return (
+      <StyledButton as={btnType} {...props}>
+          {buttonInner}
+      </StyledButton>
+    );
 }
+Button.defaultProps = {
+    isLoading: false,
+    loadingText: null,
+    isLink: false,
+    appearance: APPEARANCES.tertiary,
+    isDisabled: false,
+    isUnclickable: false,
+    containsIcon: false,
+    size: SIZES.medium,
+    ButtonWrapper: undefined,
+};
+
 export default Button;
