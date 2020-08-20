e9d4027b6ef6e3ea5f5c70b1374c468bdf0fe691
JuoJuo <36804937+JuoJuo@users.noreply.github.com>
2020-08-20 14:19:01 +0000

    Deploying to gh-pages from  @ aa7cad93dd5dd96675bf22d66a6014468e169d13 🚀

---
+++ precache-manifest.b6082ad7087973696e2548b853a23f1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacedca507361e3a0440037527432140",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3715ecfdcdbf865509b2.bundle.js"
+  },
+  {
+    "url": "runtime~main.3715ecfdcdbf865509b2.bundle.js"
+  },
+  {
+    "url": "vendors~main.3715ecfdcdbf865509b2.bundle.js"
+  },
+  {
+    "url": "vendors~main.3715ecfdcdbf865509b2.bundle.js.LICENSE.txt"
+  }
+]);
